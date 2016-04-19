8242ab5bc3ae1ae4f5ab99dd687b502c3888f12c
Abhishek Gupta <awittlegupta@gmail.com>
2016-04-19 10:39:14 -0700

    final commit for now

--- www/js/app.js
+++ www/js/app.js
@@ -1,56 +1,66 @@
-(function() {
+(function () {
 
-var app = angular.module('onepercent', ['ionic', 'angularMoment'])
+
+var app = angular.module('onepercent', 
+  [
+  'ionic', 
+  'angularMoment'
+  ]);
+
 
 app.controller("OnePercentCtrl", function($scope, $http) {
-  $scope.stories = [];
+  $scope.posts = [];
 
-  function loadStories (params, callback) {
-      $http.get("https://www.reddit.com/r/lifehacks/new/.json", {params: params})
-        .success(function(response) {
-          var stories = [];
-          angular.forEach(response.data.children, function(child){
-            stories.push(child.data);
-          });
-          callback(stories);
-      });    
+
+  function showPosts (params, callback) {
+    $http.get("https://www.reddit.com/r/lifehacks/new/.json", {params: params}).success(function(response) {
+      var posts = [];
+      angular.forEach(response.data.children, function (child){
+        posts.push(child.data);
+      });
+      callback(posts);
+    });    
   };
 
-  $scope.loadOlderStories = function () {
+
+  $scope.showOldPosts = function () {
       var params={};
-      if ($scope.stories.length > 0) {
-        params['after'] = $scope.stories[$scope.stories.length - 1].name;
+      if ($scope.posts.length > 0) {
+        params['after'] = $scope.posts[$scope.posts.length - 1].name;
       }
-      loadStories(params, function(olderStories){
-        $scope.stories = $scope.stories.concat(olderStories);
+      showPosts(params, function (pastPosts) {
+        $scope.posts = $scope.posts.concat(pastPosts);
         $scope.$broadcast('scroll.infiniteScrollComplete');
       });
 
-      $scope.openLink = function(url) {
+      $scope.newTab = function (url) {
         window.open(url, '_blank');
       };
   };
-  $scope.loadNewerStories = function () {
-    var params={'before': $scope.stories[0].name};
-      loadStories(params, function(newerStories) {
-        $scope.stories = newerStories.concat($scope.stories);
+
+
+  $scope.showNewPosts = function () {
+    var params={'before': $scope.posts[0].name};
+      showPosts(params, function(newPosts) {
+        $scope.posts = newPosts.concat($scope.posts);
         $scope.$broadcast('scroll.infiniteScrollComplete');
       });
   };
 });
 
-app.run(function($ionicPlatform) {
+
+app.run(function ($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
 
-    if(window.cordova && window.cordova.InAppBrowser) {
+    if (window.cordova && window.cordova.InAppBrowser) {
       window.open = window.cordova.InAppBrowser.open;
     }
 
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
